cc94d3d9301f68d08711e8e077abb8f6d9f8754f
alivin <540355718@qq.com>
2014-12-24 11:00:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KaalBlog::Application.config.secret_key_base = '0104cbe5bac40aa3dd4351e785b5d90483b6c7680953432e0cb8d425dbca34f7c9b8f00d013292557857164563f5d32414188e4ae37fc9d4d903c593f56f228f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KaalBlog::Application.config.session_store :cookie_store, key: '_kaal_blog_session'
